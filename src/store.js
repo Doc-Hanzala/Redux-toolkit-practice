a54d749f0f36d8cfed481abe7b98853fc3e99553
hanzala tanveer <810hanzalatanveer@gmail.com>
2024-03-13 03:55:34 -0700

    first commit

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import cartReducer from "./features/cart/cart";
+
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
